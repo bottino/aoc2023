c5aea0de3e487205787e218f0fecccaf814816a8
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-12-06 19:38:55 +0100

    Finished day 6 with brute force

--- 6/waitforit.py
+++ 6/waitforit.py
@@ -8,6 +8,10 @@ def test_parse_input():
     assert parse_input("example.txt") == [(7, 9), (15, 40), (30, 200)]
 
 
+def test_parse_input_part2():
+    assert parse_input_part2("example.txt") == Race(71530, 940200)
+
+
 def test_get_race_distance():
     races = parse_input("example.txt")
     for hold_time, expected_distance in [(0, 0), (1, 6), (2, 10), (3, 12), (4, 12), (5, 10), (6, 6), (7, 0)]:
@@ -20,14 +24,25 @@ def test_get_number_of_winning_combinations():
 
 
 def test_get_power_winning_combinations():
-    races = parse_input("input.txt")
+    races = parse_input("example.txt")
     assert get_power_of_winning_combinations(races) == 288
+
+
+def test_part_2():
+    race = parse_input_part2("input.txt")
+    assert get_number_of_winning_combinations(race) == 71503
 
 
 def parse_input(filepath: str) -> list[Race]:
     lines = [l for l in open(filepath, "r")]
     lines = [[int(n) for n in re.findall("\d+", line)] for line in lines]
     return [Race(*x) for x in zip(*lines)]
+
+
+def parse_input_part2(filepath: str) -> Race:
+    lines = [l for l in open(filepath, "r")]
+    lines = [int("".join(re.findall("\d+", line))) for line in lines]
+    return Race(*lines)
 
 
 def get_race_distance(t_hold: int, race: Race) -> int:
