2c4fe8421a7eade3f4f6d9066889e70ff593926c
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-12-06 20:03:37 +0100

    Property implemented day 6

--- 6/waitforit.py
+++ 6/waitforit.py
@@ -1,3 +1,4 @@
+import math
 import re
 from collections import namedtuple
 
@@ -5,35 +6,29 @@ Race = namedtuple("Race", "t_max record")
 
 
 def test_parse_input():
-    assert parse_input("example.txt") == [(7, 9), (15, 40), (30, 200)]
+    assert parse_input_part1("example.txt") == [(7, 9), (15, 40), (30, 200)]
 
 
 def test_parse_input_part2():
     assert parse_input_part2("example.txt") == Race(71530, 940200)
 
 
-def test_get_race_distance():
-    races = parse_input("example.txt")
-    for hold_time, expected_distance in [(0, 0), (1, 6), (2, 10), (3, 12), (4, 12), (5, 10), (6, 6), (7, 0)]:
-        assert get_race_distance(hold_time, races[0]) == expected_distance
-
-
 def test_get_number_of_winning_combinations():
-    races = parse_input("example.txt")
+    races = parse_input_part1("example.txt")
     assert get_number_of_winning_combinations(races[2]) == 9
 
 
 def test_get_power_winning_combinations():
-    races = parse_input("example.txt")
+    races = parse_input_part1("example.txt")
     assert get_power_of_winning_combinations(races) == 288
 
 
 def test_part_2():
-    race = parse_input_part2("input.txt")
+    race = parse_input_part2("example.txt")
     assert get_number_of_winning_combinations(race) == 71503
 
 
-def parse_input(filepath: str) -> list[Race]:
+def parse_input_part1(filepath: str) -> list[Race]:
     lines = [l for l in open(filepath, "r")]
     lines = [[int(n) for n in re.findall("\d+", line)] for line in lines]
     return [Race(*x) for x in zip(*lines)]
@@ -45,15 +40,22 @@ def parse_input_part2(filepath: str) -> Race:
     return Race(*lines)
 
 
-def get_race_distance(t_hold: int, race: Race) -> int:
-    v = t_hold
-    t_race = race.t_max - t_hold
-    return v * t_race
-
-
 def get_number_of_winning_combinations(race: Race) -> int:
-    w = [get_race_distance(t, race) > race.record for t in range(race.t_max + 1)]
-    return sum(w)
+    roots = find_roots_for_time_equation(race)
+    return (roots[1] - roots[0]) + 1
+
+
+def find_roots_for_time_equation(race: Race):
+    # L'equation a resoudre est t * (t_max - t) = record => t * t - t_max * t + record = 0
+    a = 1.0
+    b = float(-race.t_max)
+    c = float(race.record)
+    delta = b * b - 4.0 * a * c
+    roots = [(-b + factor * math.sqrt(delta)) / (2.0 * a) for factor in (-1, 1)]
+
+    # The lowest number of the winning range must be the integer just above the smaller root, the highest number just
+    # below the higher root
+    return (int(roots[0]) + 1, int(roots[1] - 0.00001))
 
 
 def get_power_of_winning_combinations(races):
@@ -61,3 +63,13 @@ def get_power_of_winning_combinations(races):
     for race in races:
         power *= get_number_of_winning_combinations(race)
     return power
+
+
+if __name__ == "__main__":
+    # Part 1
+    races_part1 = parse_input_part1("input.txt")
+    print(get_power_of_winning_combinations(races_part1))
+
+    # Part 2
+    race_part2 = parse_input_part2("input.txt")
+    print(get_number_of_winning_combinations(race_part2))
