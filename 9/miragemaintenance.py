c103751f4a8d338bfeab22033423edcb53d40f7b
Baptiste Ottino <baptiste.ottino@mindmaze.ch>
2023-12-10 17:16:17 +0100

    Finished day 9 part 2

--- 9/miragemaintenance.py
+++ 9/miragemaintenance.py
@@ -3,13 +3,15 @@ import numpy as np
 
 
 def test_predicted_values():
-    values = predict_values("example.txt")
-    assert values == [18, 28, 68, -18]
+    forward, backward = predict_values("example.txt")
+    assert forward == [18, 28, 68, -18]
+    assert backward == [-3, 0, 5, 3]
 
 
 def predict_values(filepath: str):
     lines = [re.findall(r"-?\d+", line) for line in open(filepath, "r")]
-    return [predict(line) for line in lines]
+    predictions = [predict(line) for line in lines]
+    return [list(pred) for pred in zip(*predictions)]
 
 
 def predict(line):
@@ -18,8 +20,13 @@ def predict(line):
     while any([number != 0 for number in pile[-1]]):
         pile.append(pile[-1][1:] - pile[-1][:-1])
 
-    return sum([seq[-1] for seq in pile])
+    forward = sum([seq[-1] for seq in pile])
+    backward = 0
+    for i in range(2, len(pile) + 1):
+        backward = pile[-i][0] - backward
+    return forward, backward
 
 
 if __name__ == "__main__":
-    print(sum(predict_values("input.txt")))
+    f, b = predict_values("input.txt")
+    print(sum(f), sum(b))
